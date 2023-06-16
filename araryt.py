97bb71313b2a099d3aa1a834152a3cc5cf22096b
laptop <JCheung262@outlook.com>
2023-06-16 14:30:32 +0100

    ??

--- araryt.py
+++ araryt.py
@@ -1,12 +1,3 @@
-class Question():
-    def __init__(self):
-        self.a=0
-        self.question=[("#use modulus to find the remainder when a=7 and is divided by 3","a=7"),("%","=","4"),("1") ]
-    def insert_self_code(self,string):
-        for i in self.question[0]:
-            string+=i
-            return string
-ques=Question()
 a="hello"
-a=ques.insert_self_code(a)
-print(a)
+if "he" in a:
+    print("Hello")
